c9a1476c8be561f9be79ba3353c01f505e6d39d8
Natasha <75773163+natty-light@users.noreply.github.com>
2024-04-16 14:54:13 -0400

    Bring AST, Parser over from QS2

---
+++ ast/ast.go
@@ -0,0 +1,518 @@
+package ast
+
+import (
+	"bytes"
+	"fmt"
+	"llvm-lang/token"
+	"strings"
+)
+
+// Interfaces
+
+type (
+	Node interface {
+		TokenLiteral() string
+		String() string
+	}
+
+	Stmt interface {
+		Node
+		statementNode()
+	}
+
+	Expr interface {
+		Node
+		expressionNode()
+	}
+)
+
+// Node
+type (
+	Program struct {
+		Stmts []Stmt
+	}
+)
+
+// Statements
+type (
+	VarDeclarationStmt struct {
+		Token    token.Token // token.Mut or token.Const
+		Name     *Identifier
+		Value    Expr
+		Constant bool
+	}
+
+	ReturnStmt struct {
+		Token       token.Token
+		ReturnValue Expr
+	}
+
+	ExpressionStmt struct {
+		Token token.Token
+		Expr  Expr
+	}
+
+	BlockStmt struct {
+		Token token.Token
+		Stmts []Stmt
+	}
+
+	VarAssignmentStmt struct {
+		Token      token.Token
+		Identifier *Identifier
+		Value      Expr
+	}
+
+	ForStmt struct {
+		Token     token.Token
+		Condition Expr
+		Body      *BlockStmt
+	}
+)
+
+// Expressions and literals
+type (
+	// Literals
+	IntegerLiteral struct {
+		Token token.Token
+		Value int64
+	}
+
+	BooleanLiteral struct {
+		Token token.Token
+		Value bool
+	}
+
+	FunctionLiteral struct {
+		Token      token.Token
+		Parameters []*Identifier
+		Body       *BlockStmt
+		Name       string
+	}
+
+	StringLiteral struct {
+		Token token.Token
+		Value string
+	}
+
+	ArrayLiteral struct {
+		Token    token.Token
+		Elements []Expr
+	}
+
+	NullLiteral struct {
+		Token token.Token
+	}
+
+	HashLiteral struct {
+		Token token.Token
+		Pairs map[Expr]Expr
+	}
+
+	FloatLiteral struct {
+		Token token.Token
+		Value float64
+	}
+
+	MacroLiteral struct {
+		Token      token.Token
+		Parameters []*Identifier
+		Body       *BlockStmt
+	}
+
+	// Expressions
+	Identifier struct {
+		Token token.Token // token.Ident
+		Value string
+	}
+
+	PrefixExpr struct {
+		Token    token.Token
+		Operator string
+		Right    Expr
+	}
+
+	InfixExpr struct {
+		Token    token.Token
+		Left     Expr
+		Operator string
+		Right    Expr
+	}
+
+	IfExpr struct {
+		Token       token.Token
+		Condition   Expr
+		Consequence *BlockStmt
+		Alternative *BlockStmt
+	}
+
+	CallExpr struct {
+		Token     token.Token
+		Function  Expr
+		Arguments []Expr
+	}
+
+	IndexExpr struct {
+		Token token.Token
+		Left  Expr
+		Index Expr
+	}
+)
+
+// Node interfaces
+func (p *Program) TokenLiteral() string {
+	if len(p.Stmts) > 0 {
+		return p.Stmts[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (v *VarDeclarationStmt) TokenLiteral() string {
+	return v.Token.Literal
+}
+
+func (r *ReturnStmt) TokenLiteral() string {
+	return r.Token.Literal
+}
+
+func (e *ExpressionStmt) TokenLiteral() string {
+	return e.Token.Literal
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (i *IntegerLiteral) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (p *PrefixExpr) TokenLiteral() string {
+	return p.Token.Literal
+}
+
+func (i *InfixExpr) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (b *BooleanLiteral) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (i *IfExpr) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (b *BlockStmt) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (f *FunctionLiteral) TokenLiteral() string {
+	return f.Token.Literal
+}
+
+func (c *CallExpr) TokenLiteral() string {
+	return c.Token.Literal
+}
+
+func (v *VarAssignmentStmt) TokenLiteral() string {
+	return v.Token.Literal
+}
+
+func (s *StringLiteral) TokenLiteral() string {
+	return s.Token.Literal
+}
+
+func (a *ArrayLiteral) TokenLiteral() string {
+	return a.Token.Literal
+}
+
+func (i *IndexExpr) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (n *NullLiteral) TokenLiteral() string {
+	return n.Token.Literal
+}
+
+func (f *ForStmt) TokenLiteral() string {
+	return f.Token.Literal
+}
+
+func (h *HashLiteral) TokenLiteral() string {
+	return h.Token.Literal
+}
+
+func (f *FloatLiteral) TokenLiteral() string {
+	return f.Token.Literal
+}
+
+func (m *MacroLiteral) TokenLiteral() string {
+	return m.Token.Literal
+}
+
+// Statements
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, stmt := range p.Stmts {
+		out.WriteString(stmt.String())
+	}
+
+	return out.String()
+}
+
+func (v *VarDeclarationStmt) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(v.TokenLiteral() + " ")
+	out.WriteString(v.Name.String())
+	out.WriteString(" = ")
+
+	if v.Value != nil {
+		out.WriteString(v.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (r *ReturnStmt) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(r.TokenLiteral() + " ")
+
+	if r.ReturnValue != nil {
+		out.WriteString(r.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (e *ExpressionStmt) String() string {
+	if e.Expr != nil {
+		return e.Expr.String()
+	}
+	return ""
+}
+
+func (b *BlockStmt) String() string {
+	var out bytes.Buffer
+
+	for _, stmt := range b.Stmts {
+		out.WriteString(stmt.String())
+	}
+
+	return out.String()
+}
+
+func (f *ForStmt) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("for (")
+	out.WriteString(f.Condition.String())
+	out.WriteString(") {")
+	out.WriteString(f.Body.String())
+	out.WriteString("}")
+
+	return out.String()
+}
+
+// Expressions
+func (i *Identifier) String() string {
+	return i.Value
+}
+
+func (p *PrefixExpr) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(p.Operator)
+	out.WriteString(p.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (i *InfixExpr) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(i.Left.String())
+	out.WriteString(" " + i.Operator + " ")
+	out.WriteString(i.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (c *CallExpr) String() string {
+	var out bytes.Buffer
+	args := make([]string, 0)
+	for _, arg := range c.Arguments {
+		args = append(args, arg.String())
+	}
+
+	out.WriteString(c.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (v *VarAssignmentStmt) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(v.Identifier.String())
+	out.WriteString(" = ")
+	out.WriteString(v.Value.String())
+
+	return out.String()
+}
+
+func (i *IndexExpr) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(i.Left.String())
+	out.WriteString("[")
+	out.WriteString(i.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
+func (i *IfExpr) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(i.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(i.Consequence.String())
+
+	if i.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(i.Alternative.String())
+	}
+
+	return out.String()
+}
+
+// Literals
+func (i *IntegerLiteral) String() string {
+	return i.Token.Literal
+}
+
+func (s *StringLiteral) String() string {
+	return s.Token.Literal
+}
+
+func (a *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+
+	for _, el := range a.Elements {
+		elements = append(elements, el.String())
+	}
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
+
+func (n *NullLiteral) String() string {
+	return "null"
+}
+
+func (b *BooleanLiteral) String() string {
+	return b.Token.Literal
+}
+
+func (f *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := make([]string, 0)
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(f.TokenLiteral())
+	if f.Name != "" {
+		out.WriteString(fmt.Sprintf("<%s>", f.Name))
+	}
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(f.Body.String())
+
+	return out.String()
+}
+
+func (h *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, val := range h.Pairs {
+		pairs = append(pairs, key.String()+":"+val.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+
+func (f *FloatLiteral) String() string {
+	return f.Token.Literal
+}
+
+func (m *MacroLiteral) String() string {
+	var out bytes.Buffer
+
+	params := make([]string, 0)
+
+	for _, p := range m.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(m.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(m.Body.String())
+
+	return out.String()
+}
+
+// Statements
+func (v *VarDeclarationStmt) statementNode() {}
+func (r *ReturnStmt) statementNode()         {}
+func (e *ExpressionStmt) statementNode()     {}
+func (b *BlockStmt) statementNode()          {}
+func (v *VarAssignmentStmt) statementNode()  {}
+func (f *ForStmt) statementNode()            {}
+
+// Expressions
+func (i *Identifier) expressionNode()      {}
+func (i *IntegerLiteral) expressionNode()  {}
+func (p *PrefixExpr) expressionNode()      {}
+func (i *InfixExpr) expressionNode()       {}
+func (b *BooleanLiteral) expressionNode()  {}
+func (i *IfExpr) expressionNode()          {}
+func (f *FunctionLiteral) expressionNode() {}
+func (c *CallExpr) expressionNode()        {}
+func (s *StringLiteral) expressionNode()   {}
+func (a *ArrayLiteral) expressionNode()    {}
+func (i *IndexExpr) expressionNode()       {}
+func (n *NullLiteral) expressionNode()     {}
+func (h *HashLiteral) expressionNode()     {}
+func (f *FloatLiteral) expressionNode()    {}
+func (m *MacroLiteral) expressionNode()    {}
---
+++ parser/parser.go
@@ -0,0 +1,639 @@
+package parser
+
+import (
+	"fmt"
+	"llvm-lang/ast"
+	"llvm-lang/lexer"
+	"llvm-lang/token"
+	"strconv"
+)
+
+type (
+	prefixParseFn func() ast.Expr
+	infixParseFn  func(ast.Expr) ast.Expr
+)
+
+type Precedence int
+
+const (
+	LOWEST Precedence = iota + 1
+	ANDOR             // I think this is right
+	EQUALS
+	LESSGREATEREQUAL
+	LESSGREATER
+	SUM
+	PRODUCT
+	PREFIX
+	CALL
+	INDEX
+)
+
+var precedences = map[token.TokenType]Precedence{
+	token.And:                ANDOR,
+	token.Or:                 ANDOR,
+	token.EqualTo:            EQUALS,
+	token.NotEqualTo:         EQUALS,
+	token.LessThan:           LESSGREATER,
+	token.GreaterThan:        LESSGREATER,
+	token.GreaterThanEqualTo: LESSGREATEREQUAL,
+	token.LessThanEqualTo:    LESSGREATEREQUAL,
+	token.Plus:               SUM,
+	token.Minus:              SUM,
+	token.Slash:              PRODUCT,
+	token.Star:               PRODUCT,
+	token.Modulo:             PRODUCT,
+	token.LeftParen:          CALL,
+	token.LeftSquareBracket:  INDEX,
+}
+
+type Parser struct {
+	lexer *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+
+	errors []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{lexer: l, errors: make([]string, 0)}
+
+	// peekToken and currToken are initialized to the zero value of token.Token, so we advance twice
+	p.nextToken() // set peek
+	p.nextToken() // set curr and peek
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+
+	p.registerPrefix(token.Identifier, p.parseIdentifier)
+	p.registerPrefix(token.Number, p.parseFloatLiteral)
+	p.registerPrefix(token.Bang, p.parsePrefixExpr)
+	p.registerPrefix(token.Minus, p.parsePrefixExpr)
+	// p.registerPrefix(token.True, p.parseBooleanLiteral)
+	// p.registerPrefix(token.False, p.parseBooleanLiteral)
+	p.registerPrefix(token.LeftParen, p.parseGroupedExpr)
+	// p.registerPrefix(token.If, p.parseIfExpr)
+	// p.registerPrefix(token.Func, p.parseFunctionLiteral)
+	p.registerPrefix(token.String, p.parseStringLiteral)
+	p.registerPrefix(token.LeftSquareBracket, p.parseArrayLiteral)
+	p.registerPrefix(token.LeftCurlyBracket, p.parseHashLiteral)
+
+	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	p.registerInfix(token.Plus, p.parseInfixExpr)
+	p.registerInfix(token.Minus, p.parseInfixExpr)
+	p.registerInfix(token.Slash, p.parseInfixExpr)
+	p.registerInfix(token.Star, p.parseInfixExpr)
+	p.registerInfix(token.Modulo, p.parseInfixExpr)
+	p.registerInfix(token.EqualTo, p.parseInfixExpr)
+	p.registerInfix(token.NotEqualTo, p.parseInfixExpr)
+	p.registerInfix(token.GreaterThanEqualTo, p.parseInfixExpr)
+	p.registerInfix(token.LessThanEqualTo, p.parseInfixExpr)
+	p.registerInfix(token.GreaterThan, p.parseInfixExpr)
+	p.registerInfix(token.LessThan, p.parseInfixExpr)
+	p.registerInfix(token.And, p.parseInfixExpr)
+	p.registerInfix(token.Or, p.parseInfixExpr)
+	p.registerInfix(token.LeftParen, p.parseCallExpr)
+	p.registerInfix(token.LeftSquareBracket, p.parseIndexExpr)
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("Honk! no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+// advances current and peek by one
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+// Checks whether current token matches given type
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.currToken.Type == t
+}
+
+// checks whether peek token matches given type
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Checks if peek token matches given type, advances tokens if true
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken() // eats
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Honk! expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) peekPrecedence() Precedence {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
+
+func (p *Parser) currPrecedence() Precedence {
+	if p, ok := precedences[p.currToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
+
+// Parsing methods
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Stmts = make([]ast.Stmt, 0)
+
+	for !p.currTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Stmts = append(program.Stmts, stmt)
+		}
+		p.nextToken() // advance past semis?
+	}
+	return program
+}
+
+// Statements
+func (p *Parser) parseStatement() ast.Stmt {
+	switch p.currToken.Type {
+	// case token.Mut:
+	// 	fallthrough
+	// case token.Const:
+	// 	return p.parseVarDeclarationStmt()
+	// case token.Return:
+	// 	return p.parseReturnStmt()
+	case token.Identifier:
+		if p.peekTokenIs(token.Assign) {
+			return p.parseAssignmentStmt()
+		} else {
+			return p.parseExpressionStmt()
+		}
+	// case token.For:
+	// 	return p.parseForStmt()
+	default:
+		return p.parseExpressionStmt()
+	}
+}
+
+// func (p *Parser) parseVarDeclarationStmt() *ast.VarDeclarationStmt {
+
+// 	// To be here, currToken is either Mut or Const
+// 	isConst := p.currToken.Type == token.Const
+
+// 	stmt := &ast.VarDeclarationStmt{Token: p.currToken, Constant: isConst}
+
+// 	// expectPeek eats?
+// 	if !p.expectPeek(token.Identifier) {
+// 		return nil
+// 	}
+
+// 	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+// 	if p.peekTokenIs(token.Semicolon) {
+// 		if isConst {
+// 			p.errors = append(p.errors, fmt.Sprintf("Honk! const variable must be initialized on line %d", p.currToken.Line))
+// 			return nil
+// 		} else {
+// 			p.nextToken() // advance past semi
+// 			// I am unsure about creating this token here, but it's not being added to the list of tokens, so it should
+// 			// be fine
+// 			stmt.Value = &ast.NullLiteral{Token: token.Token{Literal: "null", Type: token.Null, Line: p.currToken.Line}}
+// 			return stmt
+// 		}
+// 	}
+
+// 	if !p.expectPeek(token.Assign) {
+// 		return nil
+// 	}
+
+// 	p.nextToken() // advance past =
+// 	stmt.Value = p.parseExpression(LOWEST)
+
+// 	if fl, ok := stmt.Value.(*ast.FunctionLiteral); ok {
+// 		fl.Name = stmt.Name.Value
+// 	}
+
+// 	if p.peekTokenIs(token.Semicolon) {
+// 		p.nextToken()
+// 	}
+
+// 	return stmt
+// }
+
+func (p *Parser) parseReturnStmt() *ast.ReturnStmt {
+	stmt := &ast.ReturnStmt{Token: p.currToken}
+
+	p.nextToken()
+
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.Semicolon) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpressionStmt() *ast.ExpressionStmt {
+	stmt := &ast.ExpressionStmt{Token: p.currToken}
+
+	stmt.Expr = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.Semicolon) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseBlockStmt() *ast.BlockStmt {
+	block := &ast.BlockStmt{Token: p.currToken}
+	block.Stmts = make([]ast.Stmt, 0)
+
+	p.nextToken() // advance past {
+
+	for !p.currTokenIs(token.RightCurlyBracket) && !p.currTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Stmts = append(block.Stmts, stmt)
+		}
+		p.nextToken() // what is this advance for
+	}
+	return block
+}
+
+func (p *Parser) parseAssignmentStmt() ast.Stmt {
+
+	ident := &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.Assign) {
+		return nil
+	}
+	p.nextToken() // advance past =
+
+	stmt := &ast.VarAssignmentStmt{Identifier: ident, Token: ident.Token}
+	val := p.parseExpression(LOWEST) // Maybe this should be LOWEST, but since the only thing lower than ASSIGNMENT is LOWEST i think we are ok
+	stmt.Value = val
+
+	if !p.expectPeek(token.Semicolon) {
+		return nil
+	}
+
+	return stmt
+}
+
+// Expressions
+func (p *Parser) parseExpression(precedence Precedence) ast.Expr {
+	prefix := p.prefixParseFns[p.currToken.Type] // look for prefix function for p.currToken
+	if prefix == nil {
+		p.noPrefixParseFnError(p.currToken.Type)
+		return nil
+	}
+
+	left := prefix() // call prefix function
+
+	// if the statement has not ended and the passed in precedence is lower than the precedence of the next token
+	// if the precedence of the next token is higher, then we need to parse it as an infix expression because it is higher priority
+	// otherwise we return the expression as parsed by the prefix
+	for !p.peekTokenIs(token.Semicolon) && precedence < p.peekPrecedence() {
+		// look for an infix parse fn
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return left
+		}
+
+		p.nextToken()
+
+		// we bind left to the infix expression
+		left = infix(left)
+	}
+
+	return left
+}
+
+// prefix and infix functions
+
+// this is an prefixParseFn, so it will not call p.nextToken() at the end
+func (p *Parser) parseIdentifier() ast.Expr {
+	return &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+}
+
+// this is an prefixParseFn, so it will not call p.nextToken() at the end
+func (p *Parser) parseIntegerLiteral() ast.Expr {
+	literal := &ast.IntegerLiteral{Token: p.currToken}
+
+	value, err := strconv.ParseInt(p.currToken.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.currToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	literal.Value = value
+
+	return literal
+}
+
+// this is an prefixParseFn, so it will not call p.nextToken() at the end
+func (p *Parser) parsePrefixExpr() ast.Expr {
+	expr := &ast.PrefixExpr{Token: p.currToken, Operator: p.currToken.Literal}
+
+	p.nextToken() // advance past operator
+	expr.Right = p.parseExpression(PREFIX)
+
+	return expr
+}
+
+// this is an infixParseFn, so it will not call p.nextToken() at the end
+func (p *Parser) parseInfixExpr(left ast.Expr) ast.Expr {
+	expr := &ast.InfixExpr{Token: p.currToken, Operator: p.currToken.Literal, Left: left}
+
+	precedence := p.currPrecedence()
+	p.nextToken()
+	expr.Right = p.parseExpression(precedence)
+
+	return expr
+}
+
+// func (p *Parser) parseBooleanLiteral() ast.Expr {
+// 	return &ast.BooleanLiteral{Token: p.currToken, Value: p.currTokenIs(token.True)}
+// }
+
+// this is a prefixParseFn
+func (p *Parser) parseGroupedExpr() ast.Expr {
+	p.nextToken() // advance past (
+
+	expr := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RightParen) {
+		return nil
+	}
+
+	return expr
+}
+
+// func (p *Parser) parseIfExpr() ast.Expr {
+// 	expr := &ast.IfExpr{Token: p.currToken}
+
+// 	if !p.expectPeek(token.LeftParen) {
+// 		return nil
+// 	}
+// 	p.nextToken() // advance past (
+// 	expr.Condition = p.parseExpression(LOWEST)
+
+// 	if !p.expectPeek(token.RightParen) {
+// 		return nil
+// 	}
+// 	if !p.expectPeek(token.LeftCurlyBracket) {
+// 		return nil
+// 	}
+
+// 	expr.Consequence = p.parseBlockStmt()
+
+// 	// For else if, probably want to set the alternative to another ast.IfExpr, maybe by calling this method recursively
+// 	if p.peekTokenIs(token.Else) {
+// 		p.nextToken() // advance past else
+// 		if !p.expectPeek(token.LeftCurlyBracket) {
+// 			return nil
+// 		}
+
+// 		expr.Alternative = p.parseBlockStmt()
+// 	}
+
+// 	return expr
+// }
+
+func (p *Parser) parseFunctionLiteral() ast.Expr {
+	function := &ast.FunctionLiteral{Token: p.currToken}
+
+	if !p.expectPeek(token.LeftParen) {
+		return nil
+	}
+
+	function.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LeftCurlyBracket) {
+		return nil
+	}
+
+	function.Body = p.parseBlockStmt()
+
+	return function
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	idents := make([]*ast.Identifier, 0)
+
+	if p.peekTokenIs(token.RightParen) {
+		p.nextToken()
+		return idents
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+	idents = append(idents, ident)
+
+	// This loop will start with currToken equal to an ident
+	for p.peekTokenIs(token.Comma) {
+		p.nextToken() // advance comma to currToken
+		p.nextToken() // advance past comma to next ident
+		ident := &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+		idents = append(idents, ident)
+	}
+
+	if !p.expectPeek(token.RightParen) {
+		return nil
+	}
+
+	return idents
+}
+
+func (p *Parser) parseCallExpr(function ast.Expr) ast.Expr {
+	expr := &ast.CallExpr{Token: p.currToken, Function: function}
+	expr.Arguments = p.parseExpressionList(token.RightParen)
+	return expr
+}
+
+func (p *Parser) parseCallArguments() []ast.Expr {
+	args := make([]ast.Expr, 0)
+
+	if p.peekTokenIs(token.RightParen) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()                                  // advance past openParen
+	args = append(args, p.parseExpression(LOWEST)) // parse first arg
+
+	for p.peekTokenIs(token.Comma) {
+		p.nextToken() // advance comma into currToken
+		p.nextToken() // advance past comma
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RightParen) {
+		return nil
+	}
+
+	return args
+}
+
+func (p *Parser) parseStringLiteral() ast.Expr {
+	return &ast.StringLiteral{Token: p.currToken, Value: p.currToken.Literal}
+}
+
+func (p *Parser) parseArrayLiteral() ast.Expr {
+	array := &ast.ArrayLiteral{Token: p.currToken}
+
+	array.Elements = p.parseExpressionList(token.RightSquareBracket)
+
+	return array
+}
+
+func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expr {
+	list := []ast.Expr{}
+
+	if p.peekTokenIs(end) {
+		p.nextToken()
+		return list
+	}
+	p.nextToken() // advance past opening toke
+
+	list = append(list, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.Comma) {
+		p.nextToken() // advance to commma
+		p.nextToken() // advance past comma
+		list = append(list, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(end) {
+		return nil
+	}
+
+	return list
+}
+
+func (p *Parser) parseIndexExpr(left ast.Expr) ast.Expr {
+	expr := &ast.IndexExpr{Token: p.currToken, Left: left}
+
+	p.nextToken() // advance past [
+
+	expr.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RightSquareBracket) {
+		return nil
+	}
+
+	return expr
+}
+
+func (p *Parser) parseNullLiteral() ast.Expr {
+	return &ast.NullLiteral{Token: p.currToken}
+}
+
+func (p *Parser) parseForStmt() *ast.ForStmt {
+	forStmt := &ast.ForStmt{Token: p.currToken}
+	if !p.expectPeek(token.LeftParen) {
+		return nil
+	}
+	p.nextToken() // advance past for
+
+	forStmt.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RightParen) {
+		return nil
+	}
+	if !p.expectPeek(token.LeftCurlyBracket) {
+		return nil
+	}
+
+	forStmt.Body = p.parseBlockStmt()
+
+	return forStmt
+}
+
+func (p *Parser) parseHashLiteral() ast.Expr {
+	hash := &ast.HashLiteral{Token: p.currToken}
+	hash.Pairs = make(map[ast.Expr]ast.Expr)
+
+	for !p.peekTokenIs(token.RightCurlyBracket) {
+		p.nextToken()
+		key := p.parseExpression(LOWEST)
+		if !p.expectPeek(token.Colon) {
+			return nil
+		}
+		p.nextToken()
+		val := p.parseExpression(LOWEST)
+
+		hash.Pairs[key] = val
+
+		if !p.peekTokenIs(token.RightCurlyBracket) && !p.expectPeek(token.Comma) {
+			return nil
+		}
+	}
+
+	if !p.expectPeek(token.RightCurlyBracket) {
+		return nil
+	}
+
+	return hash
+}
+
+func (p *Parser) parseFloatLiteral() ast.Expr {
+	literal := &ast.FloatLiteral{Token: p.currToken}
+
+	value, err := strconv.ParseFloat(p.currToken.Literal, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as float", p.currToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	literal.Value = value
+
+	return literal
+}
+
+func (p *Parser) parseMacroLiteral() ast.Expr {
+	macro := &ast.MacroLiteral{Token: p.currToken}
+
+	if !p.expectPeek(token.LeftParen) {
+		return nil
+	}
+
+	macro.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LeftCurlyBracket) {
+		return nil
+	}
+
+	macro.Body = p.parseBlockStmt()
+
+	return macro
+}
